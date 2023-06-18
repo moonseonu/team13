94ef56517bd607dfe1a93be6d38030d76aaa3d3d
Moonseonu <foroz.fl@gmail.com>
2023-06-19 01:46:00 +0900

    미니게임부분 코드 완성
    살붙이는 단계에서 조립만 잘 하면 됨

--- Assets/Operator.cs
+++ Assets/Operator.cs
@@ -6,9 +6,12 @@ public class Operator : MonoBehaviour
 {
     LineRenderer lr;
     Vector3 room1Pos, mousePos;
-
+    Ray2D ray;
+    RaycastHit2D hit;
+    private GameObject target;
     private void Start()
     {
+        
         lr = GetComponent<LineRenderer>();
         lr.startWidth = .05f;
         lr.endWidth = .05f;
@@ -22,10 +25,16 @@ public class Operator : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            lr.SetPosition(0, room1Pos);
-            lr.SetPosition(1, mousePos);
+            CastRay();
+            if (target == this.gameObject)
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                ray = new Ray2D(mousePos, Vector2.zero);
+
+                lr.SetPosition(0, room1Pos);
+                lr.SetPosition(1, mousePos);
+            }
         }
 
         if (Input.GetMouseButton(0))
@@ -37,9 +46,31 @@ public class Operator : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            lr.SetPosition(1, GameObject.Find("Room5").GetComponent<Transform>().position);
+            CastRay();
+            if(target == GameObject.Find("Room5"))
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                lr.SetPosition(1, GameObject.Find("Room5").GetComponent<Transform>().position);
+            }
+            else
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                lr.SetPosition(1, room1Pos);
+            }
+        }
+    }
+
+    void CastRay()
+    {
+        target = null;
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+        if (hit.collider != null)
+        {
+            target = hit.collider.gameObject; 
         }
     }
 }
